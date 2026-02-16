1151f10dd666ba6761d645ae3115e0716a08d780
Dennis Adam <28174344+Piket95@users.noreply.github.com>
2026-02-16 15:54:43 +0100

    dry run script fix

--- dry_run_events.sh
+++ dry_run_events.sh
@@ -3,5 +3,10 @@
 # Wait 5 minutes before running
 sleep 300
 
+# Get directory of this script
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+cd $DIR
+
 source .venv/bin/activate
 python events.py
